c16a15fdc121b68196ca697c89b14635f37b45d2
yf-ashu <yfcai1227@gmail.com>
2018-05-14 20:12:30 +0800

    h5 falf

---
+++ answers/hw5/README.md
@@ -0,0 +1,10 @@
+## 自我練習
+
+1. HTTP method 有哪幾個？有什麼不一樣？
+2. `GET` 跟 `POST` 有哪些區別，可以試著舉幾個例子嗎？
+3. 什麼是 RESTful API？
+4. JSON 是什麼？
+5. JSONP 是什麼？
+6. 要如何存取跨網域的 API？
+
+Demo:https://yf-ashu.github.io/frontend-intermediate-course/answers/hw4/
--- answers/hw5/main.js
+++ answers/hw5/main.js
@@ -1,4 +1,4 @@
-var connect = function () {
+let connect = function () {
     fetch('https://api.twitch.tv/kraken/streams/?limit=20&language=zh-tw', {
             method: 'get',
             headers: {
@@ -16,16 +16,39 @@ var connect = function () {
             console.log('錯誤:', err);
         });
 }
+let imageLoad = (datas) => {
+    let imgSrc = document.querySelectorAll('.display img');
+    datas.forEach((data, index) => {
+        imgSrc[index].style.opacity=0;
 
-var update = (jsondata => {
+        let image = new Image();
+        let loadimg = data.preview.large;
+        image.onload = function () {
+            imgSrc[index].style.opacity=1;
+
+            imgSrc[index].src = this.src;
+            console.log(this.style);
+        };
+        image.src =data.preview.large;
+    })
+
+    // image.src ="https://static-cdn.jtvnw.net/ttv-static/404_preview-320x180.jpg";
+    // var image = document.images[0];
+    // var downloadingImage = new Image();
+    // downloadingImage.onload = function(){
+    //      image.src = this.src;   
+    // };
+    // downloadingImage.src = "http://an.image/to/aynchrounously/download.jpg";
+}
+let update = (jsondata => {
     let datas = (jsondata);
     let list = '';
     datas.forEach((data, index) => {
         list += ` <div class="block">
                         <a  target="_blank" href="${datas[index].channel.url}">
                          <div class="display">
-                             <img src="${datas[index].preview.large}" alt="">
-                        </div>
+                         <img src="https://static-cdn.jtvnw.net/ttv-static/404_preview-320x180.jpg" />
+                         </div>
                          <div class="person">
                             <div class="image">
                                  <img src="${datas[index].channel.logo}" alt="">
@@ -37,15 +60,18 @@ var update = (jsondata => {
                       </div>
                       </a>
                      </div>`;
-        console.log(datas[index]);
+        // console.log(datas[index]);
+        // imageLoad(data, index);
     });
     const dataList = document.querySelector('.wrapper ');
     dataList.innerHTML = list;
-    const creat=document.createElement('div');//要增加的屬性
-    creat.setAttribute('class','block');
+    imageLoad(datas);
+    const creat = document.createElement('div'); //要增加的屬性
+    creat.setAttribute('class', 'block');
     dataList.appendChild(creat);
 
 });
+
 
 window.onload = function () {
     connect();
